705944fca6f0edd922244538fbaaa1df4165e032
Guayo Mena <guayo.mena@gmail.com>
2019-10-24 22:38:20 -0600

    Create pages with createManagedPages

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,6 +5,27 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const axios = require("axios");
+
+function slugify(string) {
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+}
+
 module.exports = function (api) {
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
@@ -13,4 +34,20 @@ module.exports = function (api) {
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
   })
+
+  api.createManagedPages(async ({ createPage }) => {
+    const { data } = await axios.get("https://api.tronalddump.io/tag");
+
+    data._embedded.forEach(tag => {
+      const slug = `/${slugify(tag)}/`;
+      createPage({
+        path: slug,
+        component: "./src/templates/Tag.vue",
+        context: {
+          title: tag,
+          slug
+        }
+      });
+    });
+  });
 }
---
+++ src/templates/Tag.vue
@@ -0,0 +1,5 @@
+<template>
+  <Layout>
+    <h1 v-html="$context.title" />
+  </Layout>
+</template>
