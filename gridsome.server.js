e8c75b1889a10925d38ba0601e59eb6fb3324d01
Guayo Mena <guayo.mena@gmail.com>
2019-10-26 11:13:35 -0600

    Simplify and use yarn

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,23 +1,5 @@
 const axios = require("axios");
-
-function slugify(string) {
-  const a =
-    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
-  const b =
-    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------";
-  const p = new RegExp(a.split("").join("|"), "g");
-
-  return string
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
-    .replace(/^-+/, "") // Trim - from start of text
-    .replace(/-+$/, ""); // Trim - from end of text
-}
+var slugify = require("slugify");
 
 module.exports = function(api) {
   api.loadSource(async actions => {
@@ -47,9 +29,5 @@ module.exports = function(api) {
         });
       }
     }
-  });
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
   });
 };
